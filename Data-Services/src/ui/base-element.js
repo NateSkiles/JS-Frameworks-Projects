63539e2e41ad6ee816fb2b4e7a88bb6d09268c0c
NateS <nateskiles94@gmail.com>
2021-08-14 10:56:26 -0700

    button init

--- Data-Services/src/ui/base-element.js
+++ Data-Services/src/ui/base-element.js
@@ -5,5 +5,18 @@ export class BaseElement {
         this.element = null; // jQuery object
     }
 
-    
+    appendToElement(el) {
+        this.createElement();
+        el.appendToElement(this.element);
+    }
+
+    createElement() {
+        let s = this.getElementString();
+        this.element = $(s);
+    }
+
+    getElementString() {
+        throw 'Please override getElementString() in base element';
+    }
+
 }
