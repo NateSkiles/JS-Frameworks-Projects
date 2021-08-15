c373a8a11f4ed3c15006e8c3a683a052bb216adc
NateS <nateskiles94@gmail.com>
2021-08-14 20:36:38 -0700

    fix

--- Data-Services/src/app.js
+++ Data-Services/src/app.js
@@ -1,37 +1,9 @@
+import $ from 'jquery';
 import { Car } from './classes/car.js';
 import { Drone } from './classes/drone.js';
 import { fleet } from './fleet-data.js';
 import { FleetDataService } from './services/fleet-data-service.js';
+import { Button } from './ui/button.js';
 
-let dataService = new FleetDataService();
-dataService.loadData(fleet);
-
-// for (let car of dataService.cars) {
-//     console.log(car.license)
-// }
-
-// for (let drone of dataService.drones) {
-//     console.log(drone.license)
-// }
-
-// for (let e of dataService.errors) {
-//     console.log(e);
-// }
-
-
-
-// let car = dataService.getCarByLicense('AT9900');
-// let cars = dataService.getCarsSortedByLicense(); 
-// for (let car of cars)
-//     console.log(car.license);
-
-// let drones = dataService.getDronesSortedByLicense(); 
-// for (let drone of drones)
-//     console.log(drone.license);
-
-
-
-let cars = dataService.filterCarsByMake('U');
-
-for (let car of cars)
-    console.log(car.make);
+let b = new Button('Click Me');
+b.getElementString($('body'));
